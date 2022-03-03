ceb60db1f47dcc62614f7c6423f24da55690d658
Asko Nomm <asko@bien.ee>
2022-03-03 17:21:03 +0100

    Only show posts on home page that are published

--- src/Controllers/SiteController.php
+++ src/Controllers/SiteController.php
@@ -17,7 +17,7 @@ class SiteController
 
     public function index(Request $request, Response $response)
     {
-        $posts = Store::get('posts');
+        $posts = Store::get('posts', ['status' => 'published']);
 
         return $response->view('themes/' . $this->getTheme() . '/home', [
             'url' => $request->url(),
